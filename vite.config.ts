d81e053b2f95b249480674fe10e375e5818be048
Mohamed Elboraideny <33972706+mohamedelboraideny@users.noreply.github.com>
2026-01-28 01:01:39 +0200

    feat: Configure Vite for production and development
    Adds a `prod` script for building and previewing in production mode.
    Updates Vite configuration to set specific ports for development and preview environments, and enables automatic opening of the browser.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/gshr-alnazla-web-app",
   server: {
-    port: 3000,
+    port: 3000, // Development Port
+    open: true
+  },
+  preview: {
+    port: 8080, // Production Port
     open: true
   }
 });
